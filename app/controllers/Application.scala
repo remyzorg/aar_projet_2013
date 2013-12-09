df24320511fafcb6d3cef90b333f226b23949e8e
remy <remy@localhost.localdomain>
2013-12-09 22:35:29 +0100

    install bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,6 +7,14 @@ import models._
 
 object Application extends Controller {
 
+
+
+  def index2 = Action {
+    var current = models.Counter.getCurrent
+    models.Counter.increment
+    Ok(views.html.home(current));
+  }
+
   def index = Action {
     var current = models.Counter.getCurrent
     models.Counter.increment
