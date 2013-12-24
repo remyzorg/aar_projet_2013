ea9a1054c3000777f7450805bd00a38a725e95bd
remy <remy@localhost.localdomain>
2013-12-24 15:04:08 +0100

    add error display on signin form

--- app/controllers/Signin.scala
+++ app/controllers/Signin.scala
@@ -9,25 +9,30 @@ import models._
 
 object Signin extends Controller {
 
-val form = Form(
+  val errors = ""
+
+  val form = Form(
     tuple(
       "email" -> text,
       "username" -> text,
       "password" -> text,
       "confirm" -> text
-    )
+    ) verifying ("Passwords must match", result => result match {
+      case (_, _, password, confirm) => password == confirm
+    })
   )
 
   def setup = Action {
-    Ok(views.html.signin());
+    Ok(views.html.signin(form));
   }
 
-
-
   def submit = Action { implicit request =>
-    val (email, username, password, confirm) = form.bindFromRequest.get
-
-    Ok("Hi %s %s".format(email, username))
+    form.bindFromRequest.fold(
+      errors => BadRequest(views.html.signin(errors)),
+      user => user match { case (email, username, password, confirm) =>
+      Ok("Hi %s %s".format(email, username)) }
+      // Redirect(routes.Application.index) // .withSession(Security.username -> user._1)
+    )
   }
 
 
