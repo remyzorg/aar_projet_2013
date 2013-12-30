b216dc275e0bf0b2f823de9211b166fdb1beca9e
remyzorg <besognet.remy@gmail.com>
2013-12-30 10:43:28 +0100

    missing files

---
+++ app/controllers/Login.scala
@@ -0,0 +1,75 @@
+package controllers
+
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import org.bson.types.ObjectId
+import models._
+
+
+
+object Auth extends Controller {
+
+  val form = Form(
+    tuple(
+      "email" -> text,
+      "password" -> text
+    ) verifying ("Wrong email or password", result =>
+      result match
+      {case (email, password) =>
+        UserModel.findByEmailPassword(email, password) match {
+          case None => false
+          case _ => true
+        }})
+  )
+
+  def setup = Action { implicit request =>
+    Ok(views.html.login(form))
+  }
+
+  def login = Action { implicit request =>
+    form.bindFromRequest.fold (
+      errors => BadRequest(views.html.login(errors)),
+      user => user match {
+        case (email, _) =>
+          Redirect(routes.Application.index)
+            .withSession(Security.username -> email)
+      }
+    )
+  }
+
+
+  def logout = Action {
+    Redirect(routes.Auth.login).withNewSession.flashing(
+      "success" -> "You are now logged out."
+    )
+  }
+
+
+
+}
+
+// trait Secured {
+
+//   def username(request: RequestHeader) =
+//     request.session.get(Security.username)
+
+//   def onUnauthorized(request: RequestHeader) =
+//     Results.Redirect(setup)
+
+//   def withAuth(f: => String => Request[AnyContent] => Result) = {
+//     Security.Authenticated(username, onUnauthorized) { user =>
+//       Action(request => f(user)(request))
+//     }
+//   }
+
+//   def withUser(f: User => Request[AnyContent] => Result) =
+//     withAuth { username => implicit request =>
+//       UserModel.findByEmail(username) match {
+//         case None => onUnauthorized(request)
+//         case user => f(user)(request)
+//       }
+//     }
+// }
