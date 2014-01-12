3daf1abe14254d12ffad2283a1de4dad9b891111
remyzorg <besognet.remy@gmail.com>
2014-01-12 20:41:17 +0100

    fix type name compiling error

--- app/controllers/Achievements.scala
+++ app/controllers/Achievements.scala
@@ -23,7 +23,7 @@ object AchievementsUnlocker {
     }
   }
 
-  def unlockSpecial(user : user) = simplyUnlockAchievement(user, Achievements.special)
+  def unlockSpecial(user : User) = simplyUnlockAchievement(user, Achievements.special)
   def unlockFirstBuy(user : User) = simplyUnlockAchievement(user, Achievements.firstBuy)
   def unlockFirstSell(user : User) = simplyUnlockAchievement(user, Achievements.firstSell)
   def unlockRiskyInvestments(user: User, rawScore: Int) =
